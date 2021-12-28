f15b8a4f6711b5cf498ab0be77506b0068a5505e
Ekaterina Gerasina <gerasinaes@mail.ru>
2021-12-28 20:30:36 +0300

    corrected

--- app/src/main/java/com/example/task2/ViewModel.kt
+++ app/src/main/java/com/example/task2/ViewModel.kt
@@ -7,27 +7,18 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import java.net.URL
 import java.util.concurrent.ExecutorService
-import java.util.concurrent.Executors
 
 class MainActivityViewModel : ViewModel() {
-    var executorService: ExecutorService = Executors.newSingleThreadExecutor()
+    private val executorService: ExecutorService = MainApp.executorService
     val bitmapData = MutableLiveData<Bitmap>()
-
- fun load(name: String) {
-
-    executorService.execute {
-        URL(name)
-            .openConnection().getInputStream().use {
-                val bitmap = BitmapFactory.decodeStream(it)
-                bitmapData.postValue(bitmap)
-            }
-        Log.i("thread of picture loading", "${Thread.currentThread()}")
+    fun load(name: String) {
+        executorService.execute {
+            URL(name)
+                .openConnection().getInputStream().use {
+                    val bitmap = BitmapFactory.decodeStream(it)
+                    bitmapData.postValue(bitmap)
+                }
+            Log.i("thread of picture loading", "${Thread.currentThread()}")
+        }
     }
-
-}
-    override fun onCleared() {
-        executorService.shutdown()
-        super.onCleared()
-    }
-
 }
